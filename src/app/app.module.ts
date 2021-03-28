9314c2d62f70445aff3e6027a18d31edc2532905
Jakob96 <jakob96@outlook.com>
2021-03-28 14:38:48 +0200

    Added components for home view, recipe list and header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    RecipeListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/recipe-list/recipe-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { RecipeService } from '../recipe.service';
+import { Recipe } from '../recipe';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+
+export class RecipeListComponent implements OnInit {
+  @Input() filter: string;
+  recipes: Recipe[] = [];
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit() {
+    this.getRecipes(this.filter, 50);
+  }
+
+  getRecipes(query, max) {
+    this.recipeService.getRecipes(query, max)
+      .subscribe(data => {
+        this.recipes = data;
+      })
+  }
+}
