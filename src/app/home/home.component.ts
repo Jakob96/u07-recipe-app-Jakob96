cab562fe7b5f69ec71251082ab3dcccbd67145d1
Jakob96 <jakob96@outlook.com>
2021-03-30 20:48:33 +0200

    Initial empty value on search

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  search: string;
+  search: string = '';
   dishType: Array<string>;
   health: Array<string>;
   selectedDish: string;
@@ -14,7 +14,7 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-      
+
   }
 
   filterDish($event) {
