e0340a54983805be49949828fd49264ce158daff
Jakob96 <jakob96@outlook.com>
2021-05-11 12:20:49 +0200

    Add sign in and sign out to header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -7,4 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent {
   headerTitle = 'Recipe app';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  isSignedIn(): boolean{
+    return (this.authService.getToken()) ? true : false;
+  }
+
+  signOut(): void {
+    this.authService.signOut().subscribe((resp) => {
+      this.router.navigate(['/']);
+    })
+    .add(() => {
+      this.authService.removeUserData();
+    });
+  };
+
+  getUserName(): string {
+    return JSON.parse(localStorage.getItem('user')).name;
+  }
 }
