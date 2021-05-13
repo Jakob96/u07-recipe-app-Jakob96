cb09487939d28fbf64c50e672ced03112aae1d4d
Jakob96 <jakob96@outlook.com>
2021-05-14 00:22:35 +0200

    Add auth guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,15 +9,16 @@ import { SaveRecipeComponent } from './save-recipe/save-recipe.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorComponent } from './error/error.component';
+import { AuthGuard } from './auth/auth.guard';
 
 //Handles the available url:s
 const routes: Routes = [
   { path: '', component:  HomeComponent },    //Root component
   { path: 'recipe/:id', component: ShowRecipeComponent },
-  { path: 'lists', component: SavedRecipesListComponent },
-  { path: 'list/:id', component: SavedRecipesComponent },
-  { path: 'save-recipe/:id', component: SaveRecipeComponent },
-  { path: 'add-list', component: AddRecipeListComponent },
+  { path: 'lists', component: SavedRecipesListComponent, canActivate: [AuthGuard] },
+  { path: 'list/:id', component: SavedRecipesComponent, canActivate: [AuthGuard] },
+  { path: 'save-recipe/:id', component: SaveRecipeComponent , canActivate: [AuthGuard]},
+  { path: 'add-list', component: AddRecipeListComponent, canActivate: [AuthGuard] },
   { path: 'sign-in', component: SignInComponent },
   { path: 'register', component: RegisterComponent },
   { path: '**', component: ErrorComponent },  //If no route match, display the error component
