cf83cc33c3249b265da0039150d5ab267a2ffb76
Jakob96 <jakob96@outlook.com>
2021-05-16 19:25:22 +0200

    Add prod env file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  edamam_api_url: 'https://api.edamam.com/search?q=',
+  edamam_app_id: process.env.EDAMAM_APP_ID,
+  edamam_app_key: process.env.EDAMAM_APP_KEY,
+  heroku_api_url: 'https://recipe-api-laravel.herokuapp.com/api/auth',
 };
